3f1d7b8b88227e814cd6636c017ec21e459aa18c
Michijoe <e2296540@cmaisonneuve.qc.ca>
2023-10-16 22:56:45 -0400

    change cors origin

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const corsOption = {
-    origin: "http://localhost:8081"
+    origin: "https://teavue.johannapenet.com"
 };
 app.use(cors(corsOption));
 
